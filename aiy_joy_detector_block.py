103764730c58e7d10bcae9890345a18bf7564b7a
Tom Young <tyoung@n.io>
2019-04-08 18:21:33 +0100

    reinnit camera after exceptions, add privacy led option

--- aiy_joy_detector_block.py
+++ aiy_joy_detector_block.py
@@ -1,31 +1,34 @@
 from picamera import PiCamera
 
+from aiy.leds import Leds
 from aiy.vision.inference import CameraInference
 from aiy.vision.models import face_detection
 
 from nio import GeneratorBlock
-from nio.properties import VersionProperty
+from nio.properties import IntProperty, VersionProperty
+from nio.util.runner import RunnerStatus
 from nio.util.threading import spawn
 from nio.signal.base import Signal
 
 
 class JoyDetection(GeneratorBlock):
 
+    privacy_led_brightness = IntProperty(
+        title='Privacy LED Brightness (0-255)',
+        default=255,
+        advanced=True)
     version = VersionProperty('0.0.1')
 
     def __init__(self):
         super().__init__()
         self.camera = None
-        self._thread = None
+        self._leds = Leds()
         self._kill = False
+        self._thread = None
 
     def configure(self, context):
         super().configure(context)
-        self.camera = PiCamera()
-        self.camera.sensor_mode = 4
-        self.camera.resolution = (1640, 1232)
-        self.camera.framerate = 15
-        self.logger.debug('camera configured')
+        self._configure_camera()
 
     def start(self):
         super().start()
@@ -34,18 +37,44 @@ class JoyDetection(GeneratorBlock):
     def stop(self):
         super().stop()
         self._kill = True
-        self.logger.debug('killing secondary thread')
+        self.logger.debug('killing secondary thread ...')
         self._thread.join()
+
+    def _configure_camera(self):
+        try:
+            self.camera = PiCamera()
+            self.camera.sensor_mode = 4
+            self.camera.resolution = (1640, 1232)
+            self.camera.framerate = 15
+            brightness = self.privacy_led_brightness()
+            if brightness > 0:
+                self._leds.update(Leds.privacy_on(brightness=brightness))
+            self.logger.debug('camera configured')
+            if self.status.is_set(RunnerStatus.warning):
+                self.set_status('ok')
+        except:
+            self.logger.exception('failed to configure camera!')
+
+    def _release_camera(self):
+        try:
+            self.camera.close()
+            self._leds.update(Leds.privacy_off())
+            self.logger.debug('camera released')
+        except:
+            self.logger.exception('failed to close camera!')
 
     def _run(self):
         while not self._kill:
             try:
-                self.logger.debug('loading inference model')
+                self.logger.debug('loading inference model ...')
                 with CameraInference(face_detection.model()) as inference:
+                    self.logger.debug('running inference ...')
                     for result in inference.run():
-                        self.logger.debug('running inference...')
                         faces = face_detection.get_faces(result)
-                        self.logger.debug('found {} faces'.format(len(faces)))
+                        if faces:
+                            #self.logger.debug(
+                                #'found {} faces'.format(len(faces)))
+                            pass
                         out = []
                         for face in faces:
                             sig = {
@@ -59,6 +88,9 @@ class JoyDetection(GeneratorBlock):
                         else:
                             break
             except:
-                self.logger.exception('something has gone wrong!')
-        self.camera.close()
-        self.logger.debug('camera released')
+                self.logger.exception('failed to get inference result!')
+                if not self.status.is_set(RunnerStatus.warning):
+                    self.set_status('warning')
+                self._release_camera()
+                self._configure_camera()
+        self._release_camera()

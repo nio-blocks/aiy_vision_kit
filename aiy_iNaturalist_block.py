c20860d8b2920ac5aa892fbe2cd4819536ca4200
Tom Young <tyoung@n.io>
2019-04-08 22:03:35 +0100

    use inference_base

--- aiy_iNaturalist_block.py
+++ aiy_iNaturalist_block.py
@@ -1,14 +1,25 @@
-import io
-from PIL import Image
-from aiy.vision.inference import ImageInference
+from enum import Enum
+from aiy.vision.inference import CameraInference
 from aiy.vision.models import inaturalist_classification
 
 from nio import Signal
-from nio.properties import VersionProperty
+from nio.properties import FloatProperty, IntProperty, SelectProperty, \
+    VersionProperty
 from .aiy_inference_base import InferenceBase
 
 
+class Models(Enum):
+
+    birds = inaturalist_classification.BIRDS
+    insects = inaturalist_classification.INSECTS
+    plants = inaturalist_classification.PLANTS
+
+
 class iNaturalist(InferenceBase):
+
+    model = SelectProperty(Models, title='Classifier Model', default=Models.birds)
+    threshold = FloatProperty(title='Minimum Score', default=0.0)
+    top_k_predictions = IntProperty(title='Return Top k Predictions', default=3)
 
     version = VersionProperty('0.1.0')
 
@@ -22,27 +33,29 @@ class iNaturalist(InferenceBase):
         super().stop()
 
     def run(self):
-        frame_buffer = io.BytesIO()
-        self.logger.debug('loading inference model ...')
-        model = inaturalist_classification.model(inaturalist_classification.BIRDS)
-        with ImageInference(model) as inference:
-            while self._running:
-                self.camera.capture(frame_buffer, format='jpeg')
-                image = Image.open(frame_buffer)
-                result = inference.run(image)
-                predictions = inaturalist_classification.get_classes(
-                    result,
-                    top_k=3,
-                    threshold=0)
-                outgoing_signals = []
-                for prediction in predictions:
-                    signal_dict = {
-                        'label': prediction[0],
-                        'confidence': prediction[1],
-                    }
-                    outgoing_signal = Signal(signal_dict)
-                    outgoing_signals.append(outgoing_signal)
-                if not self._running:
-                    break
-                self.notify_signals(outgoing_signals)
+        while self._running:
+            try:
+                self.logger.debug('loading inference model ...')
+                model = inaturalist_classification.model(self.model().value)
+                with CameraInference(model) as inference:
+                    self.logger.debug('running inference ...')
+                    for result in inference.run():
+                        predictions = inaturalist_classification.get_classes(
+                            result,
+                            top_k=self.top_k_predictions(),
+                            threshold=self.threshold())
+                        outgoing_signals = []
+                        for prediction in predictions:
+                            signal_dict = {
+                                'label': prediction[0],
+                                'score': prediction[1],
+                            }
+                            outgoing_signal = Signal(signal_dict)
+                            outgoing_signals.append(outgoing_signal)
+                        if not self._running:
+                            break
+                        self.notify_signals(outgoing_signals)
+            except:
+                self.logger.exception('failed to get inference result!')
+                self.reset_camera()
         self.release_camera()

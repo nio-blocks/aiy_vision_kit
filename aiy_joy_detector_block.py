a2893a8c5e17796ad5b749c88f2e95653b6a0e11
Tom Young <tyoung@n.io>
2019-04-08 11:52:33 -0600

    add aiy_inference_base class

--- aiy_joy_detector_block.py
+++ aiy_joy_detector_block.py
@@ -1,67 +1,9 @@
-from picamera import PiCamera
-
-from aiy.leds import Leds
-from aiy.vision.inference import CameraInference
 from aiy.vision.models import face_detection
 
-from nio import GeneratorBlock
-from nio.properties import IntProperty, VersionProperty
-from nio.util.runner import RunnerStatus
-from nio.util.threading import spawn
-from nio.signal.base import Signal
+from aiy_inference_base import InferenceBase
 
 
-class JoyDetection(GeneratorBlock):
-
-    privacy_led_brightness = IntProperty(
-        title='Privacy LED Brightness (0-255)',
-        default=255,
-        advanced=True)
-    version = VersionProperty('0.0.1')
-
-    def __init__(self):
-        super().__init__()
-        self.camera = None
-        self._leds = Leds()
-        self._kill = False
-        self._thread = None
-
-    def configure(self, context):
-        super().configure(context)
-        self._configure_camera()
-
-    def start(self):
-        super().start()
-        self._thread = spawn(self._run)
-
-    def stop(self):
-        super().stop()
-        self._kill = True
-        self.logger.debug('killing secondary thread ...')
-        self._thread.join()
-
-    def _configure_camera(self):
-        try:
-            self.camera = PiCamera()
-            self.camera.sensor_mode = 4
-            self.camera.resolution = (1640, 1232)
-            self.camera.framerate = 15
-            brightness = self.privacy_led_brightness()
-            if brightness > 0:
-                self._leds.update(Leds.privacy_on(brightness=brightness))
-            self.logger.debug('camera configured')
-            if self.status.is_set(RunnerStatus.warning):
-                self.set_status('ok')
-        except:
-            self.logger.exception('failed to configure camera!')
-
-    def _release_camera(self):
-        try:
-            self.camera.close()
-            self._leds.update(Leds.privacy_off())
-            self.logger.debug('camera released')
-        except:
-            self.logger.exception('failed to close camera!')
+class JoyDetection(InferenceBase):
 
     def _run(self):
         while not self._kill:

46cdd0692d95f6863b0fa7fdd519edec33ed0746
Tom Young <tyoung@n.io>
2019-04-09 19:43:20 +0100

    add shutter input and capture output

--- aiy_inference_base.py
+++ aiy_inference_base.py
@@ -1,16 +1,21 @@
+from io import BytesIO
 from picamera import PiCamera
-
+from PIL import Image
 from aiy.leds import Leds
 
-from nio import GeneratorBlock
+from nio import Block
+from nio.block import input, output
 from nio.properties import BoolProperty, IntProperty, VersionProperty
 from nio.util.discovery import not_discoverable
 from nio.util.runner import RunnerStatus
 from nio.util.threading import spawn
 
 
+@input('shutter', label='Shutter', default=True)
+@output('images', label='Capture')
+@output('predictions', label='Inference')
 @not_discoverable
-class InferenceBase(GeneratorBlock):
+class InferenceBase(Block):
 
     camera_framerate = IntProperty(
         title='Maximum Camera Framerate',
@@ -38,12 +43,22 @@ class InferenceBase(GeneratorBlock):
     def __init__(self):
         super().__init__()
         self.camera = None
+        self._frame_buffer = BytesIO()
         self._leds = Leds()
         self._thread = None
 
     def configure(self, context):
         super().configure(context)
         self.configure_camera()
+
+    def process_signals(self, signals):
+        self._frame_buffer.truncate(0)
+        self._frame_buffer.seek(0)
+        self.camera.capture(self._frame_buffer, format='jpeg')
+        frame = Image.open(self._frame_buffer)
+        for signal in signals:
+            signal.image = frame
+        self.notify_signals(signals, 'images')
 
     def start(self):
         super().start()
